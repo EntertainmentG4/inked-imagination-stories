16bb65f1f42649839146476ebb94e31141119073
razan-aboushi <103279516+razan-aboushi@users.noreply.github.com>
2023-07-05 12:33:16 -0700

    upload files

---
+++ frontend/src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
+import AboutUs from "./components/AboutUs";
+import ContactUs from "./components/ContactUs";
+import Home from "./components/index";
+import LogIn from "./components/LogIn";
+import SignUp from "./components/SignUp";
+
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Nav /> 
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/AboutUs" element={<AboutUs />} />
+          <Route path="/ContactUs" element={<ContactUs />} />
+          <Route path="/LogIn" element={<LogIn />} />
+          <Route path="/SignUp" element={<SignUp />} />
+        </Routes>
+        <Footer /> 
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/reducers/index.js
@@ -0,0 +1,11 @@
+// reducers/index.js
+import { combineReducers } from 'redux';
+
+
+
+
+const rootReducer = combineReducers({
+
+});
+
+export default rootReducer;
