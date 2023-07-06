cf491dfc885bed951800becb02dda54437c8792d
razan-aboushi <103279516+razan-aboushi@users.noreply.github.com>
2023-07-06 10:16:42 -0700

    Update files

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,9 +1,42 @@
-import React from 'react'
-
-function Footer() {
+const Footer = () => {
   return (
-    <div>Footer</div>
-  )
+      <>
+          <footer className="bg-gray-400 text-blac px-4 py-5 md:px-8">
+              <div className="max-w-lg sm:mx-auto sm:text-center">
+                  <img src="" alt="Logo" className="w-32 sm:mx-auto" />
+
+                  <p className="leading-relaxed mt-2 text-[15px]">
+                      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                  </p>
+              </div>
+
+              <ul className="items-center justify-center mt-8 space-y-5 sm:flex sm:space-x-4 sm:space-y-0">
+                  <li className="text-gray-700 hover:text-gray-900">
+                      Home
+                  </li>
+
+                  <li className="text-gray-700 hover:text-gray-900">
+                      Blog
+                  </li>
+
+                  <li className="text-gray-700 hover:text-gray-900">
+                      Who are we
+                  </li>
+
+                  <li className="text-gray-700 hover:text-gray-900">
+                      get in touch
+                  </li>
+              </ul>
+
+              <div className="mt-8 items-center justify-center sm:flex">
+                  <div className="mt-4 sm:mt-0">
+                      &copy; 2022 Float UI All rights reserved.
+                  </div>
+              </div>
+
+          </footer>
+      </>
+  );
 }
 
-export default Footer
+export default Footer;
--- frontend/src/components/SignUp.js
+++ frontend/src/components/SignUp.js
@@ -77,11 +77,11 @@ const SignUp = () => {
     setSignupState({ ...signupState, [e.target.name]: e.target.value });
   };
   return (
-    <section>
-      <div className="flex flex-col items-center  px-9 py-8 mx-auto mt-32 sm:mt-0 md:h-screen lg:py-0">
+    <section className="mt-5">
+      <div className="flex flex-col items-center  px-9 py-8 mx-auto mt-32 sm:mt-0 md:h-screen lg:py-0 text-left">
         <div className="w-full bg-gray-800 border border-gray-700 rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-100 md:text-2xl">
+            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-100 md:text-2xl text-center mb-5">
               Create an Account
             </h1>
             <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
